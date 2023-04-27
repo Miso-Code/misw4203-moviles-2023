60354b064c96264d852845ed76a40dd4667b41d5
Brayan Henao <b.henaoc@uniandes.edu.co>
2023-04-27 15:44:23 -0500

    feat: Setup workflows and linter (pre-commit) options (#14)
    * Setup workflows and lint
    
    Co-authored-by: Felipe Cerquera <a.cerquera@uniandes.edu.co>
    
    * Setup pre commit
    
    Co-authored-by: Felipe Cerquera <a.cerquera@uniandes.edu.co>
    
    ---------
    
    Co-authored-by: Felipe Cerquera <a.cerquera@uniandes.edu.co>

--- app/src/main/java/com/example/misw4203_moviles_2023/data/AlbumRepository.kt
+++ app/src/main/java/com/example/misw4203_moviles_2023/data/AlbumRepository.kt
@@ -5,8 +5,8 @@ import com.example.misw4203_moviles_2023.data.network.AlbumService
 
 class AlbumRepository {
     private val api = AlbumService()
-    suspend fun getAllAlbums():List<AlbumModel>{
-        val response:List<AlbumModel> = api.getAlbums()
+    suspend fun getAllAlbums(): List<AlbumModel> {
+        val response: List<AlbumModel> = api.getAlbums()
         return response
     }
 }
